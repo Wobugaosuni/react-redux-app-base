69653196ef1a11d9dcdc9a42b3c7228861f845a9
qiuxia <397401374@qq.com>
2017-05-25 16:13:51 +0800

    bugfix about build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -44,7 +44,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: 'css-loader!postcss-loader'
