057c085024a75d28030bb7933f07f07e00d49d08
qiuxia <397401374@qq.com>
2018-07-01 15:19:52 +0800

    add component template

---
+++ template/component/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './index.styl';
+
+import Input from '../../components/Input';
+import List from '../../components/List';
+
+// 性能优化
+import PureReanderMixin from 'react-addons-pure-render-mixin';
+
+class {{componentName}} extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.shouldComponentUpdate = PureReanderMixin.shouldComponentUpdate.bind(this);
+
+    this.state = {
+      todoList: []
+    };
+  }
+
+  render() {
+    return (
+      <div role="containers:{{componentName}}">
+        <h2>To Do List Demo</h2>
+        <Input onInputSubmit={this.onInputSubmit.bind(this)} />
+        <List todoList={this.state.todoList} deleteItem={this.deleteItem.bind(this)} />
+      </div>
+    );
+  }
+
+  onInputSubmit(inputValue) {
+    let id = this.state.todoList.length;
+
+    this.setState({
+      todoList: this.state.todoList.concat({
+        id: id,
+        value: inputValue
+      })
+    });
+  }
+
+  deleteItem(id) {
+    this.setState({
+      todoList: this.state.todoList.filter((element, index) => {
+        return id !== index;
+      })
+    });
+  }
+}
+
+export default {{componentName}};
